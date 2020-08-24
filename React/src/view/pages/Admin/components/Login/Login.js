06b07471446d0d537db42e46be380bfff31620f3
Sizar <simaan.sizaar@gmail.com>
2020-08-24 15:39:51 +0300

    Auto stash before merge of "Sizar" and "origin/mhran"

--- React/src/view/pages/Admin/components/Login/Login.js
+++ React/src/view/pages/Admin/components/Login/Login.js
@@ -1,14 +1,44 @@
-import React  from 'react';
+import { useHistory } from "react-router-dom";
+import React from 'react';
 import './Login.css';
 import {
     Link
   } from "react-router-dom";
 
-function Login(props) {
- 
 
-    
-    
+
+function Login(e) {
+    e.preventDefault();
+
+    const { Email, Password } = e.target.elements;
+    const email = Email.value;
+    const password = Password.value;
+
+    fetch("/api/users/login", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            const {success} = data;
+            if(success==true){
+                const {info} = data;
+                console.log(info.role)
+            }
+            else{
+                const {error} = data
+                console.log(error)
+            }
+            if (loggedin) {
+                history.push("/main");
+            } else {
+                document.getElementById("badmsg").innerText = "User Not Exist's";
+            }
+        });
+}
     return (
         <div className='login'>
             <h3>Welcome to Jiraph System</h3>
