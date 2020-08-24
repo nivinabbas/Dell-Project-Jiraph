b9fff139635c04381b6c7baf1bfdebfc041f4175
Nimer <nemrsh1@gmail.com>
2020-08-24 11:18:45 +0300

    added routing
    each group has there own pages

--- React/src/App.js
+++ React/src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+          <li>
+              <Link to="/">Log In</Link>
+            </li>
+          <li>
+              <Link to="/register">Register</Link>
+            </li>
+          <li>
+              <Link to="/forgotPassword">Admin</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+            <li>
+              <Link to="/analysis">Analysis</Link>
+            </li>
+            <li>
+              <Link to="/status">Status</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        <Route exact={true} path="/">
+            <h1>login</h1>
+          </Route>
+          <Route path="/register">
+          <h1>register</h1>
+          </Route>
+          <Route path="/forgotPassword">
+          <h1>forgotPassword</h1>
+          </Route>
+          <Route path="/admin">
+          <h1>Admin</h1>
+          </Route>
+          <Route path="/analysis">
+          <h1>Analysis</h1>
+          </Route>
+          <Route path="/status">
+          <h1>status</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
