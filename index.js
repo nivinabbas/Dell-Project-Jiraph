e3ce73549ab4e3923605dc21277f4814d3ecdc60
Yousef Khatib <yousefkh97@gmail.com>
2020-08-24 11:46:42 +0300

    stam

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-
+    
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
