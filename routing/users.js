0462b33a4fd886f442e68fcb15b8c7974352b5a3
Yousef Khatib <yousefkh97@gmail.com>
2020-08-25 12:02:21 +0300

    very best update

--- routing/users.js
+++ routing/users.js
@@ -1,27 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const schemas = require('../index')
+const User = require('../schemas/UserSchema')
+
+
 //app.get/post/put/delete => router.get/post/put/delete
 router.post('/login',(req,res)=>{
     const {email , password} = req.body;
     console.log('emmail ', email)
-    UserModel.find({}).then(docs=>{
-        console.log(docs)
+    User.find({employeeEmail:email}).then(checkEmail=>{
+        if(checkEmail.length>0){
+            User.find({employeeEmail:email,password:password}).then(checkPassword=>{
+                if(checkPassword.length>0){
+                    res.send({success:true,error:"",info:null})
+                }else{
+                    res.send({success:false,error:"Password incorrect",info:null})
+                }
+            })
+        }else{
+            res.send({success:false,error:"Email not found",info:null})
+        }
     })
-//     UserModel.find({employeeEmail:email}).then(checkEmail=>{
-//         console.log('hello')
-//         if(checkEmail.length>0){
-//             UserModel.find({employeeEmail:email,password:password}).then(checkPassword=>{
-//                 if(checkPassword.length>0){
-//                     res.send({success:true,error:"",info:null})
-//                 }else{
-//                     res.send({success:false,error:"Password incorrect",info:null})
-//                 }
-//             })
-//         }else{
-//             res.send({success:false,error:"Email not found",info:null})
-//         }
-//     })
  })
 
 module.exports = router;
