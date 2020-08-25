fe28c617eaac5fadbd711ef365c07eb9c083fcf4
Lina <nijemlina@gmail.com>
2020-08-25 15:46:40 +0300

    check

--- routing/status.js
+++ routing/status.js
@@ -1,6 +1,11 @@
-const express = require("express");
 const router = express.Router();
 
+const express = require("express");
+const mongoose = require('mongoose');
+
+const router = express.Router();
+const UserSchema = require('../schemas/UserSchema');
+const UserModel = mongoose.model("UserModel", UserSchema)
 
 //app.get/post/put/delete => router.get/post/put/delete
 
