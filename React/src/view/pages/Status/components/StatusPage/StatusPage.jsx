be57cf9d4840f15fb55a77fe8b27df058a10208f
Morad Sleman <morad.sleman.a@gmail.com>
2020-08-25 12:24:21 +0300

    cont

--- React/src/view/pages/Status/components/StatusPage/StatusPage.jsx
+++ React/src/view/pages/Status/components/StatusPage/StatusPage.jsx
@@ -12,21 +12,31 @@ let array = [
   { name: "deleted tasks", number: 20 },
   { name: "total tasks", number: 36 },
 ];
-
 const optionSprint = [
   { value: "Backlog", label: "Backlog" },
   { value: "inProgress", label: "In Progress" },
   { value: "Done", label: "Done" },
 ];
 const StatusPage = (props) => {
-  // const [cardsContent,setCardsContent]=useState([]);
+  const [cardsContent,setCardsContent]=useState([]);
+  function getDailyAlerts(){
+    fetch('/api/status/dailyAlerts')
+    .then(res => res.json())
+    .then(data => {
+      if(data.success === false){
+        alert(data.error);
+      }
+      setCardsContent(data.content);
+    })
+  }
+  getDailyAlerts();
   // setCardsContent(array);
   // console.log(cardsContent)
 
   return (
     <div className="statuspage">
       <div className="statuspage__dashboard">
-        <DashBoard cardsContent={array} />
+        <DashBoard cardsContent={cardsContent} />
       </div>
       <div className="statuspage__table">
         <Table />
