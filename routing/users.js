e9fa8ab04e5e3ad4e771940fba3de5e350c3eb3d
Sizar <simaan.sizaar@gmail.com>
2020-08-25 15:01:18 +0300

    lrami

--- routing/users.js
+++ routing/users.js
@@ -20,23 +20,20 @@ const u = new UserModel({
 //app.get/post/put/delete => router.get/post/put/delete
 router.post('/login',(req,res)=>{
     const {email , password} = req.body;
-    console.log('email ', email)
-    UserModel.find({}).then(docs=>{
-        console.log(docs)
+    UserModel.find({"userInfo.employeeEmail":email}).then(checkEmail=>{
+        if(checkEmail.length>0){
+            
+            UserModel.find({"userInfo.employeeEmail":email,"userInfo.password":password}).then(checkPassword=>{
+                if(checkPassword.length>0){
+                    res.send({success:true,error:"",info:{role:checkPassword[0].userInfo.employeeRole}})
+                }else{
+                    res.send({success:false,error:"Password incorrect",info:null})
+                }
+            })
+        }else{
+            res.send({success:false,error:"Email not found",info:null})
+        }
     })
-    // UserModel.find({employeeEmail:email}).then(checkEmail=>{
-    //     if(checkEmail.length>0){
-    //         UserModel.find({employeeEmail:email,password:password}).then(checkPassword=>{
-    //             if(checkPassword.length>0){
-    //                 res.send({success:true,error:"",info:null})
-    //             }else{
-    //                 res.send({success:false,error:"Password incorrect",info:null})
-    //             }
-    //         })
-    //     }else{
-    //         res.send({success:false,error:"Email not found",info:null})
-    //     }
-    // })
  })
 
  router.post('/forgotPassword',(req,res)=>{
