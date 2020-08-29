f856290bdab2931fe0cdbd491ee09eef48c5a58b
Yousef Khatib <yousefkh97@gmail.com>
2020-08-29 10:52:31 +0300

    server

--- routing/analytics.js
+++ routing/analytics.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
-
+const UserSchema = require('../schemas/UserSchema');
+const UserModel = mongoose.model("UserModel", UserSchema);
+const TaskModel = require('./schemas/TaskSchema');
 
 //app.get/post/put/delete => router.get/post/put/delete
 
+
+
+router.post('/api/analytics/modificationByField', async (req, res) => {
+    const { fieldName, values, label,qaRepresentative} = req.body;
+    if(fieldName.length == 0 ){
+        let tasks = await TaskModel.aggregate([
+            { $match: {}},
+            {$group: {_id:null , fieldNames: "$diffItem.updatedField.fieldName"}}
+
+
+
+
+
+        ])
+    }
+    console.log(tasks)
+    res.send(tasks)
+})
 
 
 module.exports = router;
