091c8cd8b9b44d3ef1db3267bd7881f545aa11c4
Nivin <nivin.abbas1@gmail.com>
2020-08-29 12:39:57 +0300

    modify filters and table

--- React/src/view/pages/Status/components/Table/Table.js
+++ React/src/view/pages/Status/components/Table/Table.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./style.css";
+import SelectInput from "../Select/SelectInput";
+import DatePicker from "../Select/datePicker";
 
 /////////////Presenting Data Table/////////////
 
@@ -31,14 +33,45 @@ const getTableHeader = (item) => {
   );
 };
 
+const optionPosition = [
+  { value: "oldValue", label: "Old Value" },
+  { value: "newValue", label: "New Value" },
+];
+const optionSprint = [
+  { value: "Backlog", label: "Backlog", value: { value: true } },
+  { value: "inProgress", label: "In Progress" },
+  { value: "Done", label: "Done" },
+];
+
 const Table = ({ data }) => {
   return (
-    <table>
-      <tbody className="container">
-        {getTableHeader(dummyData[0])}
-        {getTableData(dummyData)}
-      </tbody>
-    </table>
+    <div>
+      <div className="filtersContainer">
+        <SelectInput
+          options={optionPosition}
+          dataKey="oldnewvalue"
+          //onSelect={onFilterChange}
+        />
+        <SelectInput
+          options={optionSprint}
+          dataKey="statusField"
+          // onSelect={onFilterChange}
+          isMulti={true}
+        />
+        <DatePicker />
+        {/* onDateChange={onFilterChange} */}
+        <button className="submitButton">
+          {/* onClick={fetchFilteredData} */}
+          Submit
+        </button>
+      </div>
+      <table>
+        <tbody className="container">
+          {getTableHeader(dummyData[0])}
+          {getTableData(dummyData)}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
