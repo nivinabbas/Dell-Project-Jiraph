e446b645656bfd7ebb35d6d1ddf4a44b53ffb16c
Yousef Khatib <yousefkh97@gmail.com>
2020-08-30 15:37:51 +0300

    yousef update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
-//const TaskModel = require('./schemas/TaskSchema');
+const TaskModel = require('./schemas/TaskSchema');
 
 
 
@@ -21,7 +21,44 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 // const data = []
 
 // TaskModel.insertMany(data).then(console.log('Done!'))
+let d1 = {
+    "diffItem": {
+        "updateTime": 1598574367, 
+        "type": "Update", 
+        "updatedFields": [
+            {
+                "fieldName": "qaFeatureOwner", 
+                "newValue": "", 
+                "oldValue": null
+            }
+        ]
+    }, 
+    "jiraItem": {
+        "priority": "P00", 
+        "status": "Implementing", 
+        "jiraType": "Epic", 
+        "jiraName": "PSI 41: SAR Support for FSCK/Recovery", 
+        "qaRepresentative": null, 
+        "functionalTest": "Yes", 
+        "fixVersion": "PSI_41", 
+        "jiraId": "TRIES-41773", 
+        "jiraParentId": "TRIF-842"
+    }, 
+    "qcItem": {
+        "status": "In Progress", 
+        "requirementType": "Epic", 
+        "requirementId": "2244"
+    }
+}
 
+function deleteArrays(){
+  const d2 = JSON.parse(JSON.stringify(d1));
+  const task = new TaskModel(d2)
+  console.log(task)
+   // let arr = [];
+  //  data.map((item,))
+}
+deleteArrays();
 
 const userRouter = require('./routing/users');
 app.use("/api/users", userRouter);
