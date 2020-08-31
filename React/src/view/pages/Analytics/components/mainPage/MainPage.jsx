2abf9def9b7120c7ee46973d7a838b22e1cccfcc
Saleh <salehao1996@gmail.com>
2020-08-31 09:11:35 +0300

    nimer warrnings resolved

--- React/src/view/pages/Analytics/components/mainPage/MainPage.jsx
+++ React/src/view/pages/Analytics/components/mainPage/MainPage.jsx
@@ -9,7 +9,7 @@ import {
 } from "react-router-dom";
 import ModificationByField from "../ModificationByField/ModificationByField";
 // import DeletedJiraTickets from "../DeletedJiraTickets/DeletedJiraTickets";
-// import ChangesInJiraTickets from "../ChangesInJiraTickets/ChangesInJiraTickets";
+import ChangesInJiraTickets from "../ChangesInJiraTickets/ChangesInJiraTickets";
 import ChangesByParentId from "../ChangesByParentId/ChangesByParentId";
 import DelaysInDelivery from "../DelaysInDelivery/DelaysInDelivery";
 
@@ -30,10 +30,10 @@ return(
 
             {/* <li>
               <Link className="MainForm__status--btn" to="/DeletedJiraTickets">Deleted Jiras</Link>
-            </li>
+            </li>*/}
             <li>
               <Link className="MainForm__status--btn" to="/ChangesInJiraTickets">Changes In Jira Tickets</Link>
-            </li> */}
+            </li> 
             <li>
               <Link className="MainForm__status--btn" to="/ChangesInParentID">Changes In Parent ID</Link>
             </li>
@@ -49,10 +49,10 @@ return(
           </Route>
           {/* <Route path="/DeletedJiraTickets">
             <DeletedJiraTickets/>
-          </Route>
+          </Route>*/}
           <Route path="/ChangesInJiraTickets">
              <ChangesInJiraTickets/>
-          </Route> */}
+          </Route> 
           <Route path="/ChangesInParentID">
             <ChangesByParentId/>
           </Route>
