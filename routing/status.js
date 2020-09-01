ac9cf0bf06a7af0ded310a5d86d1b28d4ac154e5
Lina <nijemlina@gmail.com>
2020-09-01 11:25:15 +0300

    new get

--- routing/status.js
+++ routing/status.js
@@ -9,6 +9,20 @@ const TaskModel = require("../schemas/TaskSchema");
 const mongoose = require("mongoose");
 let Today;
 
+////////////TEMP FUNCTIONS/////////////
+
+// to get the time format YY-MM-DD
+function dateFormat() {
+  const d = new Date();
+  const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
+  const mo = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(d);
+  const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
+
+  return `${ye}-${mo}-${da}`;
+}
+
+//////////////////////////////////////
+
 // Start daily status alert !
 router.get("/dailyalerts", async function (req, res) {
   let Today = dateFormat();
@@ -100,7 +114,6 @@ router.get("/dailyalerts", async function (req, res) {
         name: "totalTasks/NotDone",
         number: 0 + "/" + 0,
       },
-
     ];
   } else {
     DailyAlerts = [
@@ -120,44 +133,17 @@ router.get("/dailyalerts", async function (req, res) {
         name: "totalTasks/NotDone",
         number: DailyAlerts[0].totalTasks + "/" + DailyAlerts[0].NotDone,
       },
-
     ];
   }
-  // console.log("DailyAlertsStart");
-  // console.log(DailyAlerts);
-  // console.log("DailyAlertsFinal");
   res.send({ success: true, error: null, info: DailyAlerts });
 });
-
-// to get the time format YY-MM-DD
-function dateFormat() {
-  const d = new Date();
-  const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-  const mo = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(d);
-  const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
-
-  return `${ye}-${mo}-${da}`;
-}
 // End daily status alert !
 
 // start open tasks
 router.get("/openTasks", async function (req, res) {
-  TaskModel.find(
-    {
-      "taskItem.isDone": false,
-    },
-    function (err, doc) {
-      // success:T/F,error:string,info{TaskItem[Task]
-
-      res.send({
-        success: true,
-        error: null,
-        info: {
-          doc,
-        },
-      });
-    }
-  ).then((err) => console.log(err));
+  TaskModel.find({ "taskItem.isDone": false }, function (err, doc) {
+    res.send({ success: true, error: null, info: { doc } });
+  }).then((err) => console.log(err));
 });
 // end open tasks
 
@@ -165,26 +151,6 @@ router.get("/openTasks", async function (req, res) {
 router.post("/updateTasks", (req, res) => {
   console.log(req.body.jiraId);
   const { jiraId, userId } = req.body;
-
-  TaskModel.updateOne(
-    { "jiraItem.jiraId": jiraId, "taskItem.user._id": userId },
-    { $set: { "taskItem.isDone": true } },
-    function (err, doc) {
-      if (err)
-        res.send({
-          success: false,
-          error: "This task has already been completed", // or return err
-          info: { doc },
-        });
-      res.send({ success: true, error: null, info: { doc } });
-    }
-  );
-});
-// end update task
-
-async function testdate(startDate) {
-  console.log(startDate);
-  console.log("the test work");
 
   TaskModel.updateOne(
     { "jiraItem.jiraId": jiraId, "taskItem.user._id": userId },
@@ -202,52 +168,52 @@ async function testdate(startDate) {
 });
 // end update task
 
-//stackedChart start 
-
+//stackedChart start
 router.post("/stackedChart", async function (req, res) {
   let { label, datefrom, dateTo } = req.body;
   let DailyAlerts;
-  if (datefrom == null && dateTo == null)//default, label daily 
-  {
+  if (datefrom == null && dateTo == null) {
+    //default, label daily
     datefrom = new Date("2020-08-01T00:00:00.00Z");
     dateTo = new Date();
     let stackedChartDone = await TaskModel.aggregate([
       {
         $match: {
           "taskItem.updatedTime": { $gte: datefrom, $lte: dateTo },
-        }
+        },
       },
       {
         $group: {
-          _id:
-          {
+          _id: {
             $dateToString: {
               date: "$taskItem.updatedTime",
               format: "%Y-%m-%d",
             },
           },
-          "count": { $sum: 1 },
+          count: { $sum: 1 },
           done: {
             $sum: {
-              $cond: [{ $eq: ["$taskItem.isDone", true] }, 1, 0,
-              ]
+              $cond: [{ $eq: ["$taskItem.isDone", true] }, 1, 0],
             },
           },
           notDone: {
             $sum: {
-              $cond: [{ $eq: ["$taskItem.isDone", false] }, 1, 0,
-              ]
+              $cond: [{ $eq: ["$taskItem.isDone", false] }, 1, 0],
             },
           },
         },
       },
-      { $sort: { "_id": 1 } }
+      { $sort: { _id: 1 } },
     ]);
-    // adding to Done Array 
-    let DoneArray = [], NotDone = [];
-    let tempDate = [], date1 = [];
-    let tempCountDone = [], tempCountNotDone = [];
-    stackedChartDone.forEach(element => {//load data
+    // adding to Done Array
+    let DoneArray = [],
+      NotDone = [];
+    let tempDate = [],
+      date1 = [];
+    let tempCountDone = [],
+      tempCountNotDone = [];
+    stackedChartDone.forEach((element) => {
+      //load data
       tempCountDone.push(element.done);
       tempCountNotDone.push(element.notDone);
       tempDate.push(element._id);
@@ -256,56 +222,55 @@ router.post("/stackedChart", async function (req, res) {
     NotDone.push({ name: "notDone" }, { data: tempCountNotDone });
     tempCountDone = [];
     tempCountNotDone = [];
-    tempCountDone.push(DoneArray);// final array
+    tempCountDone.push(DoneArray); // final array
     tempCountDone.push(NotDone);
-    tempCountNotDone.push({ "series": tempCountDone })
-    tempCountNotDone.push({ "categories": tempDate })
+    tempCountNotDone.push({ series: tempCountDone });
+    tempCountNotDone.push({ categories: tempDate });
     finalArray = tempCountNotDone;
     res.send({ success: true, error: null, info: finalArray });
-  }
-  else {
-    datefrom = new Date(time1+"T00:00:00.00Z");
-    dateTo = new Date(time2+"T23:59:59.0099Z");
+  } else {
+    datefrom = new Date(time1 + "T00:00:00.00Z");
+    dateTo = new Date(time2 + "T23:59:59.0099Z");
     let stackedChartDone = await TaskModel.aggregate([
       {
         $match: {
           "taskItem.updatedTime": { $gte: datefrom, $lte: dateTo },
-  
-        }
+        },
       },
-  
+
       {
         $group: {
-          _id:
-          {
+          _id: {
             $dateToString: {
               date: "$taskItem.updatedTime",
               format: "%Y-%m-%d",
             },
           },
-          "count": { $sum: 1 },
+          count: { $sum: 1 },
           done: {
             $sum: {
-              $cond: [{ $eq: ["$taskItem.isDone", true] }, 1, 0,
-              ]
+              $cond: [{ $eq: ["$taskItem.isDone", true] }, 1, 0],
             },
           },
           notDone: {
             $sum: {
-              $cond: [{ $eq: ["$taskItem.isDone", false] }, 1, 0,
-              ]
+              $cond: [{ $eq: ["$taskItem.isDone", false] }, 1, 0],
             },
           },
         },
       },
-      { $sort: { "_id": 1 } }
+      { $sort: { _id: 1 } },
     ]);
 
-    // adding to Done Array 
-    let DoneArray = [], NotDone = [];
-    let tempDate = [], date1 = [];
-    let tempCountDone = [], tempCountNotDone = [];
-    stackedChartDone.forEach(element => {//load data
+    // adding to Done Array
+    let DoneArray = [],
+      NotDone = [];
+    let tempDate = [],
+      date1 = [];
+    let tempCountDone = [],
+      tempCountNotDone = [];
+    stackedChartDone.forEach((element) => {
+      //load data
       tempCountDone.push(element.done);
       tempCountNotDone.push(element.notDone);
       tempDate.push(element._id);
@@ -314,62 +279,79 @@ router.post("/stackedChart", async function (req, res) {
     NotDone.push({ name: "notDone" }, { data: tempCountNotDone });
     tempCountDone = [];
     tempCountNotDone = [];
-    tempCountDone.push(DoneArray);// final array
+    tempCountDone.push(DoneArray); // final array
     tempCountDone.push(NotDone);
-    tempCountNotDone.push({ "series": tempCountDone })
-    tempCountNotDone.push({ "categories": tempDate })
+    tempCountNotDone.push({ series: tempCountDone });
+    tempCountNotDone.push({ categories: tempDate });
     finalArray = tempCountNotDone;
 
     res.send({ success: true, error: null, info: finalArray });
-
   }
   res.send({ success: false, error: null, info: null });
 });
 
+//fieldName start
+router.get("/getFieldName", async function (req, res) {
+  TaskModel.distinct("diffItem.updatedField.fieldName", function (err, doc) {
+    // success:T/F,error:string,info{TaskItem[Task]
 
-async function test123test(time1,time2) {
-  datefrom = new Date(time1+"T00:00:00.00Z");
-  dateTo = new Date(time2+"T23:59:59.0099Z");
+    res.send({ success: true, error: null, info: { doc } });
+  }).then((err) => console.log(err));
+});
+//fieldName end
+
+//getType start
+router.get("/getType", async function (req, res) {
+  TaskModel.distinct("diffItem.type", function (err, doc) {
+    // success:T/F,error:string,info{TaskItem[Task]
+    res.send({ success: true, error: null, info: { doc } });
+  }).then((err) => console.log(err));
+});
+//getType end
+
+async function test123test(time1, time2) {
+  datefrom = new Date(time1 + "T00:00:00.00Z");
+  dateTo = new Date(time2 + "T23:59:59.0099Z");
   let stackedChartDone = await TaskModel.aggregate([
     {
       $match: {
         "taskItem.updatedTime": { $gte: datefrom, $lte: dateTo },
-
-      }
+      },
     },
 
     {
       $group: {
-        _id:
-        {
+        _id: {
           $dateToString: {
             date: "$taskItem.updatedTime",
             format: "%Y-%m-%d",
           },
         },
-        "count": { $sum: 1 },
+        count: { $sum: 1 },
         done: {
           $sum: {
-            $cond: [{ $eq: ["$taskItem.isDone", true] }, 1, 0,
-            ]
+            $cond: [{ $eq: ["$taskItem.isDone", true] }, 1, 0],
           },
         },
         notDone: {
           $sum: {
-            $cond: [{ $eq: ["$taskItem.isDone", false] }, 1, 0,
-            ]
+            $cond: [{ $eq: ["$taskItem.isDone", false] }, 1, 0],
           },
         },
       },
     },
-    { $sort: { "_id": 1 } }
+    { $sort: { _id: 1 } },
   ]);
 
-  // adding to Done Array 
-  let DoneArray = [], NotDone = [];
-  let tempDate = [], date1 = [];
-  let tempCountDone = [], tempCountNotDone = [];
-  stackedChartDone.forEach(element => {//load data
+  // adding to Done Array
+  let DoneArray = [],
+    NotDone = [];
+  let tempDate = [],
+    date1 = [];
+  let tempCountDone = [],
+    tempCountNotDone = [];
+  stackedChartDone.forEach((element) => {
+    //load data
     tempCountDone.push(element.done);
     tempCountNotDone.push(element.notDone);
     tempDate.push(element._id);
@@ -378,47 +360,14 @@ async function test123test(time1,time2) {
   NotDone.push({ name: "notDone" }, { data: tempCountNotDone });
   tempCountDone = [];
   tempCountNotDone = [];
-  tempCountDone.push(DoneArray);// final array
+  tempCountDone.push(DoneArray); // final array
   tempCountDone.push(NotDone);
-  tempCountNotDone.push({ "series": tempCountDone })
-  tempCountNotDone.push({ "categories": tempDate })
-  // console.log(tempCountNotDone)
+  tempCountNotDone.push({ series: tempCountDone });
+  tempCountNotDone.push({ categories: tempDate });
+  console.log(tempCountNotDone);
 }
 
-// test123test("2020-09-01","2020-09-02");
-//stackedChart end 
+// test123test("2020-08-25", "2020-08-31");
+//stackedChart end
+
 module.exports = router;
-
-
-
-
-
-
-
-  // $expr:
-        // {
-        //   $and: [
-        //     {
-        //       $gte: [
-        //         dateTo,
-        //         {
-        //           $dateToString: {
-        //             date: "$taskItem.updatedTime",
-        //             format: "%Y-%m-%d",
-        //           },
-        //         },
-        //       ],
-        //     },
-        //     {
-        //       "$lte": [
-        //         datefrom,
-        //         {
-        //           $dateToString: {
-        //             date: "$taskItem.updatedTime",
-        //             format: "%Y-%m-%d",
-        //           },
-        //         },
-        //       ],
-        //     }
-        //   ]
-        // },
