6538d9d44cbd932aaf8296b774f82c864a53cf5c
Taima <taimab98@gmail.com>
2020-09-02 09:21:13 +0300

    css

--- React/src/view/pages/Admin/components/Login/Login.js
+++ React/src/view/pages/Admin/components/Login/Login.js
@@ -52,15 +52,18 @@ function Login(props) {
             });
     }
     return (
-        <div className='login'>
-            <h3>Welcome to Jiraph System</h3>
+        <div className='login-wrapper'>
+            <div className="block"></div>
+            <div className="login">
+            <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;300;400;500;531;600;700;800&display=swap" rel="stylesheet"></link>
+            <h3 className="header">Welcome to Jiraph System</h3>
             <form id="loginForm" onSubmit={handleLogin} >
-                <input id="userEmail-Inp" name="userEmailInp" placeholder="Enter your Emailadress"></input>
-                <input id="userPsw-Inp" type="password" name="userPswInp" placeholder="Enter your Password"></input>
+                <input id="userEmail-Inp" name="userEmailInp" placeholder="Enter your Email Adress"></input>
+                <input  id="userPsw-Inp" type="password" name="userPswInp" placeholder="Enter your Password"></input>
                 <button type="submit">Login</button>
             </form>
-            <Link to="/forgotPassword">Forgot Password?</Link>
-
+            <Link className="forgetPassword" to="/forgotPassword">Forgot Password?</Link>
+            </div>
         </div>
     )
 }
