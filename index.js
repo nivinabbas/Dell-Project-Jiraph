b386ee189ab23ccdb73b725a43279ac7896cfd8b
Nivin <nivin.abbas1@gmail.com>
2020-09-08 16:13:02 +0300

    fix

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-    extended: true
-}));
-const port=process.env.PORT ||4000;
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
+);
+const port = process.env.PORT || 4000;
 //const TaskModel = require('./schemas/TaskSchema');
 
-const TaskModel = require('./schemas/TaskSchema');
+const TaskModel = require("./schemas/TaskSchema");
 
+app.use(express.static("public"));
 
+//const url = "mongodb+srv://nimer:N1N1N1N1@cluster0.tejcy.mongodb.net/server";
+const url =
+  "mongodb+srv://Marshood:raMHdQuDOBxwrcss@cluster0.ifcjp.mongodb.net/jira";
 
-app.use(express.static('public'))
-
- const url = "mongodb+srv://nimer:N1N1N1N1@cluster0.tejcy.mongodb.net/server";
-// const url = "mongodb+srv://Marshood:raMHdQuDOBxwrcss@cluster0.ifcjp.mongodb.net/jira";
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // const data = []
 
 // TaskModel.insertMany(data).then(console.log('Done!'))
 
-
-
-const userRouter = require('./routing/users');
+const userRouter = require("./routing/users");
 app.use("/api/users", userRouter);
 
-const statusRouter = require('./routing/status');
+const statusRouter = require("./routing/status");
 app.use("/api/status", statusRouter);
 
-const analyticsRouter = require('./routing/analytics');
+const analyticsRouter = require("./routing/analytics");
 app.use("/api/analytics", analyticsRouter);
 
-const bellaRouting = require('./routing/bellaRouting');
+const bellaRouting = require("./routing/bellaRouting");
 app.use("/api/PostBellaData", bellaRouting);
 
 app.listen(port, () => {
-    console.log("App is Listening to port:",port)
-})
+  console.log("App is Listening to port:", port);
+});
