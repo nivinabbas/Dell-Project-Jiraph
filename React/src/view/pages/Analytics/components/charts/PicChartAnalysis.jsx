db881916ba8fce2b31137025f2008fb1b84e91a0
Nimer <nemrsh1@gmail.com>
2020-09-10 23:30:28 +0300

    init

--- React/src/view/pages/Analytics/components/charts/PicChartAnalysis.jsx
+++ React/src/view/pages/Analytics/components/charts/PicChartAnalysis.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import Chart from "react-apexcharts";
+import { useState } from "react";
 
 
 const PieChartAnalysis = (props) => {
   const {UiObjs} = props;
   console.log(UiObjs)
-  const tasks=["tasks ta3on critical","tasks ta3on Major","tasks ta3on Light"]
+ const [tasks,setTasks]=useState([]);
+  
+  
+
+ 
   const options = {
-    series: [0, 55, 13],
-    labels: ['Critical Changes', 'Major Changes', 'Light Changes',],
-    colors: ['#ff0000', '#ffff00', '#00ff00'],
+    series: [UiObjs[0].featuresSize, UiObjs[1].featuresSize, UiObjs[2].featuresSize],
+    labels: ['Light Changes','Major Changes','Critical Changes'],
+    colors: ['#00ff00','#ffff00','#ff0000'],
     chart: {
       events: {
         dataPointSelection: function(event, chartContext, config) {
-          
-          console.log(tasks[config.dataPointIndex])
-          console.log(options.labels[config.dataPointIndex])
+          console.log(UiObjs[config.dataPointIndex].features)
+          // console.log([config.dataPointIndex])
+          console.log(config.dataPointIndex)
         }
       }
     }
