0c7a0adcc6f93afffa140e35ba5976780debcffe
Nimer <nemrsh1@gmail.com>
2020-09-11 12:30:00 +0300

    finished changes by parent id

--- React/src/view/pages/Analytics/components/charts/PicChartAnalysis.jsx
+++ React/src/view/pages/Analytics/components/charts/PicChartAnalysis.jsx
@@ -1,45 +1,76 @@
 import React from "react";
 import Chart from "react-apexcharts";
 import { useState } from "react";
-
-
+import "./PieChartAnalysis.css";
+import MainTable from "../MainTable/MainTable"
 const PieChartAnalysis = (props) => {
-  const {UiObjs} = props;
-  console.log(UiObjs)
- const [tasks,setTasks]=useState([]);
-  
-  
-
- 
+  const { UiObjs } = props;
+  const features = [UiObjs[0].features, UiObjs[1].features, UiObjs[2].features];
+  const [featuresToDisplay, setFeaturesToDisplay] = useState([]);
+  const [tasks,setTasks]= useState([]);
   const options = {
     series: [UiObjs[0].featuresSize, UiObjs[1].featuresSize, UiObjs[2].featuresSize],
-    labels: ['Light Changes','Major Changes','Critical Changes'],
-    colors: ['#00ff00','#ffff00','#ff0000'],
+    labels: ['Light Changes(1-10)', 'Major Changes(11-19)', 'Critical Changes(20+)'],
+    colors: ['#00ff00', '#ffff00', '#ff0000'],
     chart: {
       events: {
-        dataPointSelection: function(event, chartContext, config) {
-          console.log(UiObjs[config.dataPointIndex].features)
-          // console.log([config.dataPointIndex])
-          console.log(config.dataPointIndex)
+        dataPointSelection: function (event, chartContext, config) {
+          setFeaturesToDisplay(features[config.dataPointIndex])
+          // if(config.dataPointIndex !=null)
+          // return(
+          // setFeaturesToDisplay(features[config.dataPointIndex])
+          // )
         }
       }
     }
   }
+  const handleClick = tasks => {
+    return setTasks(tasks)
+  }
 
   return (
-    <div>
-        <Chart
-          options={options}
-          series={options.series}
-          type="pie"
-          width="500px"
-          
-        />
+    <div className="PieChartAnalysis__Wrapper">
+      <Chart className="PieChartAnalysis__Chart"
+        options={options}
+        series={options.series}
+        type="pie"
+        width="500px"
+      />
+      {featuresToDisplay && 
+      <div className="PieChartAnalysis__FeaturesTable">
+        <table className="PieChartAnalysis__FeaturesTableContainer">
+          <thead className="PieChartAnalysis__FeaturesTableHeader">
+            <tr><th scope="col">#</th>
+              <th scope="col">Feature ID</th>
+              <th scope="col">Total Changes</th> 
+              <th scope="col">Total Create</th>
+              <th scope="col">Total Update</th> 
+              <th scope="col">Total Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {featuresToDisplay.map((task, index) => (
+              <tr onClick={()=>handleClick(task.tasks)} key={index}>
+                <th scope="row"> {++index} </th>
+                <td> {task._id} </td>
+                <td> {task.TotalChanges} </td>
+                <td> {task.Create} </td>
+                <td>
+                  {task.Update}
+                </td>
+                <td>
+                  {task.Delete}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>}
+
+      {tasks.length>0 && <div className="PieChartAnalysis__MainTable"><MainTable  tasks={tasks}/></div>}
 
 
-
-
-</div>
+    </div>
   );
 };
 
