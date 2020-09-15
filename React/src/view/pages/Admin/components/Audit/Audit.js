17ac7d63eeda77bc72a76337d5ba55d1fbf0f251
Mahmoud Ataria <mahmoud.ataria95@gmail.com>
2020-09-15 12:19:57 +0300

    Audit

--- React/src/view/pages/Admin/components/Audit/Audit.js
+++ React/src/view/pages/Admin/components/Audit/Audit.js
@@ -1,40 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Audit.css'
+
+import AuditRow from './AuditRow';
+
+function UserAudit() {
+
+    // const [usersAudit, setUsersAudit] = useState([]);
+    const [users, setUsers] = useState([]);
+    
 
 
-function Audit() {
-    return(
-       
-
-            <div>
-                <div id="header">
-                </div>
+    useEffect(() => {
+        fetch('/api/users/getUsersAudit')
+            .then(res => res.json())
+            .then(data => {
                 
-                    <div className="TableColHeeader">
-                        <h4>UserEmail</h4>
-                        <h4>Business Role</h4>
-                        <h4>Action</h4>
-                        <h4>date</h4>
-                    </div>
-                    <form  >
-                        <input name="userEmail"></input>
-                        <input   name='userRole' ></input>
-                        <input name="action"></input>
-                        <input name="date"></input>
-                        
-                      
-                    </form>
-    
-    
-                    </div>
-    
-    
+                if (data.success == true) {
+                    // alert("ok");
+                    setUsers(data.info.table);
+                }
+                else {
+                    alert(data.error)
+                }
+            })
+    }, []);
+   
+    return (
+
+        <div className='adminpage'>
+            <div id="header">
+            </div>
+            <div className='AdminTable'>
+                <div className="TableColHeeader">
+                    <h4>Username</h4>
+                    <h4>E-Mail</h4>
+                    <h4> Role</h4>
+                    <h4>Action</h4>
+                    <h4>Date</h4>
+                </div>
               
-            
+                
+                {users.map(user => <AuditRow setUsers={setUsers} key={user.id} user={user} />)}
+            </div>
+
+        </div>
+    );
+
     
-    
-    
-        
-    )
 }
 
-export default Audit;
+export default UserAudit;
+
+
+
